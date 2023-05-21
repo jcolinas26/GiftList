1c113021e92cc9e28e99bb7b73976d9400b868d2
JC <jcolinas26@gmail.com>
2023-05-21 19:27:44 +0100

    Add existing project files to Git

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList');
 
 const port = 1225;
 
@@ -7,16 +9,26 @@ const app = express();
 app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
+// create the merkle tree for the whole nice list
+const merkleTree = new MerkleTree(niceList);
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+//const MERKLE_ROOT = '';
+const MERKLE_ROOT = merkleTree.getRoot();
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const name = req.body.name;
+  const proof = req.body.proof;
+  console.log(name);
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  // const isInTheList = false;
+  //--------------------------
+  // verify proof against the Merkle Root
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
+  //---------------------------
+  if (isInTheList) {
     res.send("You got a toy robot!");
   }
   else {
